17382f4eebb9f79225e9d21702e8c2eebbd7309f
A.L. Blackwell <ahvblackwelltech@gmail.com>
2020-07-08 20:00:36 -0500

    commit

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata_ahvblackwelltech",
+    version="0.0.1",
+    author="Ahvi Blackwell",
+    author_email="ahvi-blackwell@lambdastudents.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
