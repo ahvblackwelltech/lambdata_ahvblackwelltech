fcd3967042c64c6cbed7270fc66ca75628afa625
A.L. Blackwell <ahvblackwelltech@gmail.com>
2020-07-09 21:13:50 -0500

    commit

--- my_lambda/helper_utility.py
+++ my_lambda/helper_utility.py
@@ -1,17 +1,38 @@
 
 import pandas as pd
-import numpy as np
-from datetime import datetime
+from pandas_profiling import ProfileReport
 
-nba_teams = ['Bucks', 'Warriors', 
-             'Clippers', 'Lakers',
-             'Bulls', 'Celtics']
-
-''' Single function to take a list, 
-    turn it into a series and add it 
-    to a dataframe as a new column 
+''' Change column names: replace 
+    spaces with underscores
 '''
 
-def convertList():
-    '''  '''
-    return
+def changeColumnsName(df):
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', ''))
+    return df
+
+''' Train/validate/test 
+    split function for 
+    a dataframe
+'''
+
+def train_validation_test_split(X, y, train_size=0.7, 
+                                val_size=0.1, test_size=0.2, 
+                                random_state=None, shuffle=True):
+
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size),
+        random_state=random_state, shuffle=shuffle)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('burritos.csv')
+    breakpoint()
+
+    print(df.shape)
+
+    
